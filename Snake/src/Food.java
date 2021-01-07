e8bb5bfb9395e4f16621e995fcb8b6fcea464b57
LevraGav <72689610+LevraGav@users.noreply.github.com>
2021-01-07 23:25:33 +0700

    Update Food.java

--- Snake/src/Food.java
+++ Snake/src/Food.java
@@ -8,27 +8,27 @@ import java.awt.Graphics;
 import java.util.Random;
 
 public class Food {
-    private Location location; 
-    private Random random = new Random();
+    private Location location; //melakukan pemanggilan class location
+    private Random random = new Random(); //deklarasi pemanggilan fungsi random dari library java
 
     public Food() {
-        this.location = new Location(this.random.nextInt(15) * 25 + 25, this.random.nextInt(15) * 25 + 25);
+        this.location = new Location(this.random.nextInt(15) * 25 + 25, this.random.nextInt(15) * 25 + 25); //penempatan makanan ular dilakukan secara random
     }
 
-    public void render(Graphics g) {
+    public void render(Graphics g) { // mengatur tampilan dari makanan ular
         g.setColor(Color.WHITE);
         g.fillOval(this.location.getX(), this.location.getY(), 25, 25);
     }
 
-    public Location getLocation() {
+    public Location getLocation() { //mendapatkan koordinat/lokasi dari makanan, kemudian nilainya dimasukkan ke location
         return this.location;
     }
 
-    public void setLocation(Location location) {
+    public void setLocation(Location location) { 
         this.location = location;
     }
 
     public void move() {
-        this.location = new Location(this.random.nextInt(15) * 25 + 25, this.random.nextInt(15) * 25 + 25);
+        this.location = new Location(this.random.nextInt(15) * 25 + 25, this.random.nextInt(15) * 25 + 25); //pergerakan makanan ular di dalam game setelah dimakan dilakukan secara random
     }
 }
