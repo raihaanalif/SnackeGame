4ca35001c77f6596d64dd3d0bf956c3199a19013
LevraGav <72689610+LevraGav@users.noreply.github.com>
2021-01-07 23:08:24 +0700

    Update Location.java

--- Snake/src/Location.java
+++ Snake/src/Location.java
@@ -4,27 +4,27 @@
 //
 
 public class Location {
-    private int x;
-    private int y;
+    private int x; //inisiasi variabel x
+    private int y; // inisiasi variabel y
 
     public Location(int x, int y) {
-        this.x = x;
-        this.y = y;
+        this.x = x; //mengeset bahwa lokasi x adalah x
+        this.y = y; //mengeset bahwa lokasi y adalah y
     }
 
     public int getX() {
-        return this.x;
+        return this.x; //jika sudah mendapatkan koordinat x, maka nilainya di return ke getx
     }
 
     public void setX(int x) {
-        this.x = x;
+        this.x = x; //
     }
 
     public int getY() {
-        return this.y;
+        return this.y;  //jika sudah mendapatkan koordinat x, maka nilainya di return ke getx
     }
 
     public void setY(int y) {
-        this.y = y;
+        this.y = y; //
     }
 }
