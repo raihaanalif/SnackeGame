fb68de6afbaee9d2f266f139599ee371fe3cc555
LevraGav <72689610+LevraGav@users.noreply.github.com>
2021-01-07 22:32:26 +0700

    Update Main.java

--- Snake/src/Main.java
+++ Snake/src/Main.java
@@ -9,17 +9,17 @@ public class Main {
     //untuk menset ukuran window game diset 700 x 500
     public static final int WIDTH = 700;
     public static final int HEIGHT = 500;
-    private static final String TITLE = "2D Snake";
-    private static Renderer renderer;
+    private static final String TITLE = "2D Snake"; //memberi judul
+    private static Renderer renderer; //memanggil class renderer
 
     public Main() {
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("2D Snake");
-        frame.setBounds(0, 0, 700, 500);
-        frame.setResizable(false);//agar tidak dapat di resize ukuran windowsnya maka resizable di set false
-        frame.setDefaultCloseOperation(3);
+        JFrame frame = new JFrame("2D Snake"); //memunculkan dan memberi nama judul pada window game
+        frame.setBounds(0, 0, 700, 500); //mengatur batas-batas
+        frame.setResizable(false); //agar tidak dapat di resize ukuran windowsnya maka resizable di set false
+        frame.setDefaultCloseOperation(3); //menutup gambar secara default
         renderer = new Renderer();
         frame.add(renderer);
         frame.setVisible(true);
