e3d1de0bbf7b15ffd886db3caa5a8e7aaf08b0d1
raihaanalif <68364765+raihaanalif@users.noreply.github.com>
2021-01-07 22:39:28 +0700

    Update Main.java

--- Snake/src/Main.java
+++ Snake/src/Main.java
@@ -19,9 +19,9 @@ public class Main {
         JFrame frame = new JFrame("2D Snake"); //memunculkan dan memberi nama judul pada window game
         frame.setBounds(0, 0, 700, 500); //mengatur batas-batas
         frame.setResizable(false); //agar tidak dapat di resize ukuran windowsnya maka resizable di set false
-        frame.setDefaultCloseOperation(3); //menutup gambar secara default
-        renderer = new Renderer();
-        frame.add(renderer);
+        frame.setDefaultCloseOperation(3); //menutup window secara default
+        renderer = new Renderer(); //mendeklarasikan class renderer ke dalam kelas main
+        frame.add(renderer); //memasukan class renderer ke dalam JFrame
         frame.setVisible(true);
     }
 }
