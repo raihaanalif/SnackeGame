288fdb282ac7b7d4e4635de82b66ba485a85e6d2
raihaanalif <68364765+raihaanalif@users.noreply.github.com>
2021-01-10 02:44:30 +0700

    Update Food.java

--- Snake/src/Food.java
+++ Snake/src/Food.java
@@ -6,10 +6,12 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
+import java.awt.Graphics2D;
 
 public class Food {
-    private Location location; //melakukan pemanggilan class location
+    private Location location;  //melakukan pemanggilan class location
     private Random random = new Random(); //deklarasi pemanggilan fungsi random dari library java
+    public int foodColor=0;
 
     public Food() {
         this.location = new Location(this.random.nextInt(15) * 25 + 25, this.random.nextInt(15) * 25 + 25); //penempatan makanan ular dilakukan secara random
@@ -18,13 +20,30 @@ public class Food {
     public void render(Graphics g) { // mengatur tampilan dari makanan ular
         g.setColor(Color.WHITE);
         g.fillOval(this.location.getX(), this.location.getY(), 25, 25);
+        Graphics2D g2 = (Graphics2D) g;
+        if(foodColor==0) {
+            g2.setColor(Color.RED);
+            foodColor++;
+            g2.fillOval(this.location.getX(), this.location.getY(), 25, 25);
+        }
+        else if(foodColor==1){
+            g2.setColor(Color.blue);
+            foodColor++;
+            g2.fillOval(this.location.getX(), this.location.getY(), 25, 25);
+        }
+        else if (foodColor==2){
+            g2.setColor(Color.MAGENTA);
+            foodColor=0;
+            g2.fillOval(this.location.getX(), this.location.getY(), 25, 25);
+        }
+
     }
 
     public Location getLocation() { //mendapatkan koordinat/lokasi dari makanan, kemudian nilainya dimasukkan ke location
         return this.location;
     }
 
-    public void setLocation(Location location) { 
+    public void setLocation(Location location) {
         this.location = location;
     }
 
