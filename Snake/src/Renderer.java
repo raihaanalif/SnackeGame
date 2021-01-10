e1df25270dcce654b803db794ba48f61e2ba9c0a
LevraGav <72689610+LevraGav@users.noreply.github.com>
2021-01-10 16:23:16 +0700

    Update Renderer.java

--- Snake/src/Renderer.java
+++ Snake/src/Renderer.java
@@ -1,7 +1,9 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
+/**
+ * Mengatur rendering gambar di dalam game
+ *
+ * @author (Arvel Gavrilla R. , Raihan Alifianto)
+ * @version (10 - 1 - 2021)
+ */
 
 import java.awt.Color;
 import java.awt.Graphics;
