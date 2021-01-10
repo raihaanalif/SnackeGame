a2a399728d0ceaebf3f575fc45946effc55313fd
raihaanalif <68364765+raihaanalif@users.noreply.github.com>
2021-01-10 16:06:34 +0700

    Update GamePlay.java

--- Snake/src/GamePlay.java
+++ Snake/src/GamePlay.java
@@ -46,7 +46,7 @@ public class GamePlay implements ActionListener, KeyListener {
         this.gamestat = 0;
         this.dificulty = 1;
         java.net.URL titleresource = getClass().getResource("title.png");
-        this.title = new ImageIcon(titleresource);
+        this.title = new ImageIcon(titleresource); //menampilkan logo dari gamenya itu sendiri
     }
 
     public void gamestart() {
@@ -64,11 +64,13 @@ public class GamePlay implements ActionListener, KeyListener {
     public void render(Graphics2D g) {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         if (this.gamestat == 0) {
+            //menampilkan tulisan dan lain sebagainya yang diperlukan di laman menu
             g.setColor(Color.WHITE);
             this.title.paintIcon(this.renderer, g, 225, 100);
-            g.setFont(new Font("arial", 1, 20));
-            g.drawString("SPACE to Start", 275, 270);
+            g.setFont(new Font("arial", 1, 20)); //mengatur font tulisan yang ada pada menu
+            g.drawString("SPACE to Start", 275, 270); //mengatur letak tulisan yang ada di menu
             switch(this.dificulty) {
+               //mengatur berbagai tulisan dificulty yang ada di laman menu
                 case 1:
                     g.drawString("<< Dificulty: Easy >>", 245, 300);
                     break;
@@ -92,12 +94,14 @@ public class GamePlay implements ActionListener, KeyListener {
                 case 3:
                     g.drawString("HARD", 545, 125);
             }
-
+            //memasukan komponen yang diperlukan di dalam game seperti score, dificulty, panjang ular dan sebagainya
             g.drawString("Score: " + this.score, 530, 200);
             g.drawString("Length: " + this.snake.getSnakeloc().size(), 530, 225);
             if (this.gamestat == 1) {
+                //bila gamenya berjalan, maka terdapat tulisan pause di sisi kanan
                 g.drawString("SPACE to Pause", 500, 275);
             } else if (this.gamestat == 3) {
+                //bila gamenya sedang berhenti sejenak/pause maka terdapat tapilan untuk melanjutkan atau keluar dari game
                 g.drawString("SPACE to Continue", 490, 275);
                 g.drawString("ESC to Exit", 515, 300);
             }
@@ -105,10 +109,13 @@ public class GamePlay implements ActionListener, KeyListener {
             this.snake.render(g, this.renderer);
             this.food.render(g);
             if (this.gamestat == 2) {
+                //bila game over, maka terdapat tulisan keterangan game over serta dapat mengulang kebali dengan menekan spasi
                 g.setFont(new Font("arial", 1, 50));
                 g.drawString("GAME OVER", 200, 225);
                 g.setFont(new Font("arial", 1, 20));
                 g.drawString("SPACE to restart", 265, 255);
+                g.setFont(new Font("arial", 1, 20));
+                g.drawString("ESC to Exit", 295, 300);
             }
         }
 
@@ -116,12 +123,14 @@ public class GamePlay implements ActionListener, KeyListener {
 
     public void actionPerformed(ActionEvent ae) {
         if (this.gamestat == 1) {
+            //bila game berjalan dan mengenai badannya maka gamestat akan berubah statusnya menjadi 2 / game over
             if (!this.snake.move(this.move)) {
                 this.gamestat = 2;
                 this.timer.stop();
             }
 
             if (this.snake.eat(this.food)) {
+                //bila ular memakan makanannya maka panjang ular bertambah dan poinnya bertambah 10
                 this.food.move();
                 this.snake.grow();
                 this.score += 10;
@@ -137,17 +146,19 @@ public class GamePlay implements ActionListener, KeyListener {
 
     public void keyPressed(KeyEvent ke) {
         if (this.gamestat == 0) {
+            //jika gamestat statusnya 0 / masih di menu bila menekan spasi gamestat akan berubah status menjadi 1 / gamenya berjalan 
             if (ke.getKeyCode() == 32) { //ASCI spasi
                 this.gamestart();
                 this.gamestat = 1;
             }
-
+            //dimenu bila kita menekan arrow right dificulty akan berubah menjadi lebih besar/sulit 
             if (ke.getKeyCode() == 39) { //ASCI right
                 ++this.dificulty;
                 if (this.dificulty > 3) {
                     this.dificulty = 1;
                 }
             } else if (ke.getKeyCode() == 37) { //ASCII left
+                //dimenu bila menekan arrow left dificultynya akan berkurang menjadi lebih kecil/mudah
                 --this.dificulty;
                 if (this.dificulty < 1) {
                     this.dificulty = 3;
@@ -156,6 +167,7 @@ public class GamePlay implements ActionListener, KeyListener {
 
             this.renderer.repaint();
         } else if (this.gamestat == 1) {
+            //fungsi ini adalah fungsi pergerakan ular
             if (this.canmove) {
                 switch(ke.getKeyCode()) {
                     case 32:
@@ -189,17 +201,21 @@ public class GamePlay implements ActionListener, KeyListener {
                 this.canmove = false;
             }
         } else if (this.gamestat == 2) {
+            //bila gamestatus 2 / gameover maka kita dapat mengulang gamenya dengan menekan tombol spasi atau keluar dari game dengan menekan
+            //tombol esc
             if (ke.getKeyCode() == 32) {
                 this.gamestart();
                 this.gamestat = 1;
-            } else if (ke.getKeyCode() == 10) {
-                this.gamestat = 0;
+            } else if (ke.getKeyCode() == 27) {
+                System.exit(0);
             }
 
             this.renderer.repaint();
         } else if(this.gamestat == 3 && ke.getKeyCode() == 32) {
+            //bila game pause dan kita menekan spasi maka game akan berubah statusnya menjadi 1 / kembali berjalan
             this.gamestat = 1;
         } else if(this.gamestat == 3 && ke.getKeyCode() == 27){
+            //bila game pause lalu kita menekan esc maka kita akan keluar dari game
 //            this.gamestat = 0;
 //            this.dificulty = 1;
             System.exit(0);
